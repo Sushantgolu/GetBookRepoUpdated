a48a6cc4f473f85b245a5671b7ac9be85ff0a097
Sushantgolu <st.sushanttiwari44@gmail.com>
2025-05-12 08:39:36 +0530

    intial version

--- src/main/java/com/example/demo/entity/Book.java
+++ src/main/java/com/example/demo/entity/Book.java
@@ -3,16 +3,22 @@ package com.example.demo.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
 
     @Id
     @GeneratedValue
-    private Integer id;
-    private String name;
-    private String author;
-
+    public Integer id;
+    public String name;
+    public String author;
 
 
 }
